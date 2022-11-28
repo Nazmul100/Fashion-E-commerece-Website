fba1774f8edb164955f66b626e56f0ab43654914
Nazmul100 <94348112+Nazmul100@users.noreply.github.com>
2022-11-28 10:43:11 +0600

    button adding product portion

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -36,20 +36,22 @@
                     </div>
                     <div class="row col-md-12">
                             @foreach($products as $product)
-                                <div class="col-md-3">
-                                    <a href="{{ url ('/single-product') }}">
-                                        <div class="featured-item">
-                                            <img style="width: 250px; height: 250px" src="{{ asset ('images/'.$product->image)}}" alt="Item">
-                                            <h4>{{ $product->name }}</h4>
-                                            <h6>{{ $product->price }}</h6>
-                                        </div>
+                            <div class="col-md-3">
+                                <a href="{{ url ('/single-product') }}">
+                                    <div class="featured-item text-center">
+                                        <img style="width: 250px; height: 250px"
+                                             src="{{ asset ('images/'.$product->image)}}" alt="Item">
+                                        <h4>{{ $product->name }}</h4>
+                                        @if ($product->sale_price)
+                                            <h5>{{ $product->sale_price }}</h5>
+                                            <h6 style="text-decoration: line-through">${{ $product->price }}</h6>
+                                        @endif
                                         <form>
-                                            <input type="submit" class="btn btn-primary mt-3" value="Add To Cart" />
+                                            <input type="submit" class="btn btn-primary mt-3" value="Add To Cart"/>
                                         </form>
-                                    </a>
-
-
-                                </div>
+                                    </div>
+                                </a>
+                            </div>
                         @endforeach
                     </div>
                 </div>
